f24738984d72925bb3cdc143253d47b3f30f50cf
Leykun-Gizaw <Leykungizaw@gmail.com>
2025-06-25 22:10:07 +0300

    Update look of home page

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="p-[20px] flex justify-between">
-      <Link className="text-3xl font-[800]" to="/">
+    <header className="p-[20px] flex justify-between border-b border-[#AAAAAA]">
+      <Link className="text-3xl font-[900]" to="/">
         #VANLIFE
       </Link>
       <nav>
--- pages/Home.jsx
+++ pages/Home.jsx
@@ -2,13 +2,29 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <>
-      <h1>You got the travel plans, we got the travel vans.</h1>
-      <p>
-        Add adventure to your life by joining the #vanlife movement. Rent the
-        perfect van to make your perfect road trip.
-      </p>
-      <Link to="vans">Find your van</Link>
-    </>
+    <div className="flex flex-col md:flex-row gap-9 mt-24 mb-24 w-[90%] m-auto">
+      <div className="flex flex-col  h-fit gap-9">
+        <h1 className="text-4xl font-extrabold">
+          You got the travel plans, we got the travel vans.
+        </h1>
+        <p>
+          Add adventure to your life by joining the #vanlife movement. Rent the
+          perfect van to make your perfect road trip.
+        </p>
+        <Link
+          to="vans"
+          className="bg-[#FF8C38] text-white p-4 text-center rounded"
+        >
+          Find your van
+        </Link>
+      </div>
+      <div className="h-[600px] overflow-hidden">
+        <img
+          src="../assets/images/home-hero.png"
+          alt="home-hero"
+          className="rounded-lg object-cover w-full h-full"
+        />
+      </div>
+    </div>
   );
 }
